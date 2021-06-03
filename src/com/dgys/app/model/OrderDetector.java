5d67cd757f3767365c80000ff53afe0ec0ae99b5
it-dm <meng.dong@yyhj.com.cn>
2021-06-03 10:17:04 +0800

    移除未使用的包引用

--- src/com/dgys/app/model/OrderDetector.java
+++ src/com/dgys/app/model/OrderDetector.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
